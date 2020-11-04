0543a257573972b12d048fbda78d2e92252e89fd
andreaugustorossi <andreaugustorossi@gmail.com>
2020-11-04 15:02:10 -0300

    commit

--- webapp/handler.py
+++ webapp/handler.py
@@ -1,13 +1,12 @@
-import os
 import pickle
 import pandas as pd
 from flask             import Flask, request, Response
 from rossmann.Rossmann import Rossmann
 
 # loading model
-model = pickle.load( open( 'model/model_rossmann.pkl', 'rb') )
+model = pickle.load( open( r'C:\Users\arros\OneDrive\ciencia_de_dados\data_science_em_producao\Rossmann-Kaggle\model\model_rossmann.pkl', 'rb') )
 
-# initialize API
+# Initialize API
 app = Flask( __name__ )
 
 @app.route( '/rossmann/predict', methods=['POST'] )
@@ -43,5 +42,4 @@ def rossmann_predict():
         return Reponse( '{}', status=200, mimetype='application/json' )
 
 if __name__ == '__main__':
-    port = os.environ.get( 'PORT', 5000 )
-    app.run( host='127.0.0.1', port=port )
+    app.run( '127.0.0.1' )
