39c9644130a78b7b0969b708742e2921e450c964
andreaugustorossi <andreaugustorossi@gmail.com>
2020-11-05 01:16:13 -0300

    commit

--- webapp/handler.py
+++ webapp/handler.py
@@ -7,7 +7,7 @@ from rossmann.Rossmann import Rossmann
 # loading model
 model = pickle.load( open( 'model/model_rossmann.pkl', 'rb') )
 
-# Initialize API
+# initialize API
 app = Flask( __name__ )
 
 @app.route( '/rossmann/predict', methods=['POST'] )
@@ -38,9 +38,10 @@ def rossmann_predict():
         
         return df_response
         
+        
     else:
         return Reponse( '{}', status=200, mimetype='application/json' )
-   
+
 if __name__ == '__main__':
     port = os.environ.get( 'PORT', 5000 )
-    app.run( '0.0.0.0', port=port )
+    app.run( host='0.0.0.0', port=port )
