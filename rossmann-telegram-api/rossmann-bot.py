2f892ac8bda52973980cbc7466b1898ffca00d3b
andreaugustorossi <andreaugustorossi@gmail.com>
2020-11-09 15:59:06 -0300

    final

--- rossmann-telegram-api/rossmann-bot.py
+++ rossmann-telegram-api/rossmann-bot.py
@@ -1,19 +1,24 @@
+import os
 import requests
 import json
 import pandas as pd
+
 from flask import Flask, request, Response
 
 # Constants
-TOKEN = "1389763704:AAELP5r6P2S8AH410pYZOZZKw_hdIoQq5M4"
+TOKEN = '1389763704:AAELP5r6P2S8AH410pYZOZZKw_hdIoQq5M4'
 
 # Bot Info
-https://api.telegram.org/bot1389763704:AAELP5r6P2S8AH410pYZOZZKw_hdIoQq5M4/getMe
+#https://api.telegram.org/bot1389763704:AAELP5r6P2S8AH410pYZOZZKw_hdIoQq5M4/getMe
 
 # Get Updates
-https://api.telegram.org/bot1389763704:AAELP5r6P2S8AH410pYZOZZKw_hdIoQq5M4/getUpdates
+#https://api.telegram.org/bot1389763704:AAELP5r6P2S8AH410pYZOZZKw_hdIoQq5M4/getUpdates
+
+# Webhook
+#https://api.telegram.org/bot1389763704:AAELP5r6P2S8AH410pYZOZZKw_hdIoQq5M4/setWebhook?url=https://arros-33d824ec.localhost.run
 
 # Send Message
-https://api.telegram.org/bot1389763704:AAELP5r6P2S8AH410pYZOZZKw_hdIoQq5M4/sendMessage?chat_id=1269100792&text=Hi André, I am fine!
+#https://api.telegram.org/bot1389763704:AAELP5r6P2S8AH410pYZOZZKw_hdIoQq5M4/sendMessage?chat_id=1269100792&text=Hi André, I am fine!
 
 def send_message ( chat_id, text ): 
     url = 'https://api.telegram.org/bot{}/'.format( TOKEN )
@@ -27,8 +32,8 @@ def send_message ( chat_id, text ):
 
 def load_dataset( store_id ):
     # loading test dataset
-    df_store_raw = pd.read_csv( r'C:\Users\arros\OneDrive\ciencia_de_dados\data_science_em_producao\Rossmann-Kaggle\store.csv', sep=',', low_memory = False )
-    df10 = pd.read_csv( r'C:\Users\arros\OneDrive\ciencia_de_dados\data_science_em_producao\Rossmann-Kaggle\test.csv' )
+    df_store_raw = pd.read_csv( 'store.csv', sep=',', low_memory = False )
+    df10 = pd.read_csv( 'test.csv' )
 
     # merge test dataset + store
     df_test = pd.merge( df10, df_store_raw, how='left', on='Store' )
@@ -36,13 +41,17 @@ def load_dataset( store_id ):
     # choose store for prediction
     df_test = df_test[df_test['Store'] == store_id ]
 
-    # remove closed days
-    df_test = df_test[df_test['Open'] != 0]
-    df_test = df_test[~df_test['Open'].isnull()]
-    df_test = df_test.drop( 'Id', axis=1 )
+    if not df_test.empty:
+        # remove closed days
+        df_test = df_test[df_test['Open'] != 0]
+        df_test = df_test[~df_test['Open'].isnull()]
+        df_test = df_test.drop( 'Id', axis=1 )
 
-    # convert Dataframe to json
-    data = json.dumps( df_test.to_dict( orient='records' ) )
+        # convert Dataframe to json
+        data = json.dumps( df_test.to_dict( orient='records' ) )
+
+    else:
+        data = 'error'
 
     return data
 
@@ -66,6 +75,14 @@ def parse_message( message ):
     chat_id = message['message']['chat']['id']    
     store_id = message['message']['text']    
 
+    store_id.replace( '/', '' )
+
+    try:
+        store_id = int( store_id )
+
+    except ValueError: 
+        store_id = 'error'
+
     return chat_id, store_id
 
 
@@ -73,26 +90,45 @@ def parse_message( message ):
 # API Initialize
 app = Flask( __name__ ) 
 
-@app.route( '/', methods = [ 'GET', 'POST' ]
+@app.route( '/', methods=['GET', 'POST'] )
 def index():
-    if request.method == 'POST': 
-       message = requests.get_json() 
-           
-       chat_id, store_id = parse_message( message ) 
+    if request.method == 'POST':
+        message = request.get_json()
+
+        chat_id, store_id = parse_message( message )
+
+        if store_id != 'error':
+            # loading data
+            data = load_dataset( store_id )
+
+            if data != 'error':
+                # prediction
+                d1 = predict( data )
+
+                # calculation
+                d2 = d1[['store', 'prediction']].groupby( 'store' ).sum().reset_index()
+                
+                # send message
+                msg = 'Store Number {} will sell R${:,.2f} in the next 6 weeks'.format(
+                            d2['store'].values[0],
+                            d2['prediction'].values[0] ) 
+
+                send_message( chat_id, msg )
+                return Response( 'Ok', status = 200 )
+
+            else:
+                send_message( chat_id, 'Store Not Available' )
+                return Response( 'Ok', status = 200 )
+
+        else:
+            send_message( chat_id, 'Store ID is Wrong' )
+            return Response( 'Ok', status = 200 )
 
 
     else:
-       return '<h1> Rossmann Telegram BOT <h1>' 
+        return '<h1> Rossmann Telegram BOT </h1>'
 
 
-
-if __name__ == "__main__':
-    app.run( host = '0.0.0.0', port = 5000 )
-
-# Predictions by store
-#d2 = d1[['store', 'prediction']].groupby( 'store' ).sum().reset_index()
-
-#for i in range( len( d2 ) ):
-#    print( 'Store Number {} will sell R${:,.2f} in the next 6 weeks'.format( 
-#            d2.loc[i, 'store'], 
-#            d2.loc[i, 'prediction'] ) )
+if __name__ == '__main__':
+    port = os.environ.get( 'PORT', 5000 )
+    app.run( host='0.0.0.0', port = port )
